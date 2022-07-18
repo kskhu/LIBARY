208d4d4080172099e152cbdf33c8a2916d482023
kskhu <109512474+kskhu@users.noreply.github.com>
2022-07-18 11:18:06 +0200

    Add files via upload

---
+++ project2.js
@@ -0,0 +1,27 @@
+Window onsccroll=()=>{
+
+if (window.scrolly>80){
+    document.querySelector(!Header .header -2!).classList
+    add (active);
+
+    
+}else{
+
+    document.querySelector(!header .header -2!).classList.add(active);
+
+}
+if (windows .scrolly>80){
+    document.querySelect(:header . header -2).classlist.add (active)
+
+{else
+    document.queryselector (!header .header -2).classlist.remove(active)
+{
+    searchform=document.queryselector('search-form');
+
+    document.queryselector("#search-btn").onClick=()=>{
+
+        searchform.classList.toggle
+
+    }
+
+
